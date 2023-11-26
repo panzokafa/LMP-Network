a310166e3164f31842e6c9133cc3ed19e9e1e27d
Luthvv <lutfhi.r.g@gmail.com>
2023-11-26 15:57:23 +0700

    ganti strktur frontend -lutpi

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,76 @@
+<div class="biru-tua pt-5 pl-14 pb-20">
+    <div>
+        <img class="w-44 h-auto mb-10" src="{{ asset('images/logo2.png') }}" alt="">
+
+        <div class="ml-5 flex gap-44">
+            <div>
+                <div class="opensans text-xl text-white mb-4">Connect with us</div>
+
+                <div class="flex items-center gap-5 mb-4">
+                    <img class="w-8 h-auto" src="{{ asset('images/sosmed/facebook.png') }}" alt="">
+                    <img class="w-8 h-auto" src="{{ asset('images/sosmed/linkedin.png') }}" alt="">
+                    <img class="w-8 h-auto" src="{{ asset('images/sosmed/twitter.png') }}" alt="">
+                    <img class="w-8 h-auto" src="{{ asset('images/sosmed/instagram.png') }}" alt="">
+                </div>
+
+                <div class="inter text-white">
+                    © 2023 by Technology Team LMP Networks.</div>
+            </div>
+
+            <div class="flex items-center gap-7 poppins text-white">
+                <div class="flex gap-32 h-full">
+                    <div>
+                        <div class="font-bold mb-5 text-lg">HOME</div>
+
+                        <div class="flex flex-col gap-3 text-sm">
+                            <div class="cursor-pointer">About</div>
+                            <div class="cursor-pointer">Product & Service</div>
+                            <div class="cursor-pointer">Solutions</div>
+                            <div class="cursor-pointer">Partners</div>
+                            <div class="cursor-pointer">Contact</div>
+                            <div class="cursor-pointer">Support</div>
+
+                        </div>
+                    </div>
+
+                    <div class="h-full w-[1px] bg-[#D9D9D9]"></div>
+                </div>
+
+                <div class="flex gap-24 h-full">
+                    <div>
+                        <div class="font-bold mb-5 text-lg">PRODUCT</div>
+
+                        <div class="flex flex-col gap-3 text-sm">
+                            <div class="cursor-pointer">LMP Networks</div>
+                            <div class="cursor-pointer">LMP Renewable Energy</div>
+                            <div class="cursor-pointer">LMP Edge DC</div>
+                            <div class="cursor-pointer">LMP Polymer</div>
+                            <div class="cursor-pointer">LMP Service</div>
+
+                        </div>
+                    </div>
+
+                    <div class="h-full w-[1px] bg-[#D9D9D9]"></div>
+                </div>
+
+                <div class="flex gap-24 h-full">
+                    <div>
+                        <div class="font-bold mb-5 text-lg">ABOUT</div>
+
+                        <div class="flex flex-col gap-3 text-sm">
+                            <div class="cursor-pointer">About LMP</div>
+                            <div class="cursor-pointer">History LMP</div>
+                            <div class="cursor-pointer">Events</div>
+                            <div class="cursor-pointer">News Room</div>
+                            <div class="cursor-pointer">Lmp Leadership</div>
+                            <div class="cursor-pointer">Career Opportunity</div>
+                            <div class="cursor-pointer">Ethics & Complience</div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,11 @@ use App\Http\Controllers\User\HomeController;
 Route::get('admin/login', [LoginController::class, 'index'])->name('admin.login');
 Route::post('admin/login', [LoginController::class, 'authenticate'])->name('admin.login.auth');
 
-Route::group(['prefix' => 'admin','middleware' => ['admin.auth']], function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
-    Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
-    Route::get('user', [UserController::class, 'index'])->name('admin.user');
-});
+// Route::group(['prefix' => 'admin','middleware' => ['admin.auth']], function () {
+//     Route::get('/', [DashboardController::class, 'index'])->name('admin.dashboard');
+//     Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
+//     Route::get('user', [UserController::class, 'index'])->name('admin.user');
+// });
 
 
 
@@ -49,4 +49,3 @@ Route::get('/', [HomeController::class, 'index'])->name('user.home');
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
