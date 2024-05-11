c8f4997f51736182a167f9bf526863bd2e36e6b5
adhi <adzkiyadzikrullah@gmail.com>
2024-05-11 17:23:49 +0700

    update chat versi 1

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,18 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function conversations()
+    {
+
+        return $this->hasMany(Conversation::class,'sender_id')->orWhere('receiver_id',$this->id)->whereNotDeleted();
+    }
+
+    /**
+     * The channels the user receives notification broadcasts on.
+     */
+    public function receivesBroadcastNotificationsOn(): string
+    {
+        return 'users.'.$this->id;
+    }
 }
--- resources/views/livewire/chat/index.blade.php
+++ resources/views/livewire/chat/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     <div
-        class="fixed h-full flex bg-white border  lg:shadow-sm overflow-hidden inset-0 lg:top-16  lg:inset-x-2 m-auto lg:h-[90%] rounded-t-lg">
+        class="fixed h-full flex bg-white border  lg:shadow-sm overflow-hidden inset-0 lg:top-16  lg:inset-x-2 m-auto rounded-t-lg">
 
         <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border">
 
