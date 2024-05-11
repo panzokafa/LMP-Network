941635aff657ed9f12fe965fe83405b258aba81b
adhi <adzkiyadzikrullah@gmail.com>
2024-05-11 13:40:54 +0700

    update second

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -1,37 +1,35 @@
-<div
-
- class="flex flex-col transition-all h-full overflow-hidden">
+<div x-data="{type:'all'}" class="flex flex-col transition-all h-full overflow-hidden">
 
     <header class="px-3 z-10 bg-white sticky top-0 w-full py-2">
 
         <div class="border-b justify-between flex items-center pb-2">
 
             <div class="flex items-center gap-2">
-                 <h5 class="font-extrabold text-2xl">Chats</h5>
+                <h5 class="font-extrabold text-2xl">Chats</h5>
             </div>
 
-             <button>
+            <button>
 
-                <svg class="w-7 h-7"  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
-                  </svg>
+                <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+                </svg>
 
-             </button>
+            </button>
 
         </div>
 
         {{-- Filters --}}
 
- <div class="flex gap-3 items-center overflow-x-scroll p-2 bg-white">
+        <div class="flex gap-3 items-center overflow-x-scroll p-2 bg-white">
 
-    <button @click="type='all'" :class="{'bg-blue-100 border-0 text-black':type=='all'}" class="inline-flex justify-center items-center rounded-full gap-x-1 text-xs font-medium px-3 lg:px-5 py-1  lg:py-2.5 border ">
-            All
-    </button>
-    <button @click="type='deleted'" :class="{'bg-red-100 border-0 text-black':type=='deleted'}" class="inline-flex justify-center items-center rounded-full gap-x-1 text-xs font-medium px-3 lg:px-5 py-1  lg:py-2.5 border ">
-        Deleted
-    </button>
+            <button @click="type='all'" :class="{'bg-blue-100 border-0 text-black':type=='all'}" class="inline-flex justify-center items-center rounded-full gap-x-1 text-xs font-medium px-3 lg:px-5 py-1  lg:py-2.5 border ">
+                All
+            </button>
+            <button @click="type='deleted'" :class="{'bg-red-100 border-0 text-black':type=='deleted'}" class="inline-flex justify-center items-center rounded-full gap-x-1 text-xs font-medium px-3 lg:px-5 py-1  lg:py-2.5 border ">
+                Deleted
+            </button>
 
-</div>
+        </div>
 
 
     </header>
@@ -44,10 +42,9 @@
         <ul class="p-2 grid w-full spacey-y-2">
 
 
-            <li
-             class="py-3 hover:bg-gray-50 rounded-2xl dark:hover:bg-gray-700/70 transition-colors duration-150 flex gap-4 relative w-full cursor-pointer px-2 ">
+            <li class="py-3 hover:bg-gray-50 rounded-2xl dark:hover:bg-gray-700/70 transition-colors duration-150 flex gap-4 relative w-full cursor-pointer px-2 ">
                 <a href="#" class="shrink-0">
-                    <x-avatar src="https://source.unsplash.com/500x500?face}"  />
+                    <x-avatar src="https://source.unsplash.com/500x500?face}" />
                 </a>
 
                 <aside class="grid grid-cols-12 w-full">
@@ -58,7 +55,7 @@
                         <div class="flex justify-between w-full items-center">
 
                             <h6 class="truncate font-medium tracking-wider text-gray-900">
-               asd
+                                asd
                             </h6>
 
                             <small class="text-gray-700">apasaja</small>
@@ -70,20 +67,20 @@
 
                         <div class="flex gap-x-2 items-center">
 
-                                      {{-- double tick  --}}
+                            {{-- double tick  --}}
                             <span>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-all" viewBox="0 0 16 16">
-                                    <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
-                                    <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
-                                  </svg>
+                                    <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z" />
+                                    <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z" />
+                                </svg>
                             </span>
 
 
 
 
 
-                             <p class="grow truncate text-sm font-[100]">
-                               asdasd
+                            <p class="grow truncate text-sm font-[100]">
+                                asdasd
                             </p>
 
 
@@ -112,4 +109,4 @@
         </ul>
 
     </main>
-</div>
+</div>s
