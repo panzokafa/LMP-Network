8b0c0a42a1a994c8d3c18dea5e80881524cb1cbb
adhi <adzkiyadzikrullah@gmail.com>
2024-05-16 15:43:17 +0700

    update arrow

--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -28,15 +28,13 @@ Echo.private('users.{{ Auth()->User()->id }}')
 
             <div class="flex w-full items-center px-2 lg:px-4 gap-2 md:gap-5">
 
-                <a class="shrink-0 lg:hidden" href="#">
-
+                <a class="shrink-0 lg:hidden" href="/service/chat">
 
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
                     </svg>
-
 
                 </a>
 
