1df239602641cb7e8dcea21cba48267322a39bf2
adhi <adzkiyadzikrullah@gmail.com>
2024-05-17 15:27:02 +0700

    update home icon

--- resources/views/livewire/chat/chat-list.blade.php
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -29,15 +29,18 @@ Echo.private('users.{{ Auth()->User()->id }}')
                 <h5 class="font-extrabold text-2xl">Chats</h5>
             </div>
 
-            <button>
+            @php
+                $role = auth()->user()->role; // Asumsikan Anda mengambil peran pengguna yang sedang login
+            @endphp
 
-                <svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    viewBox="0 0 16 16">
-                    <path
-                        d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
+            <button onclick="location.href='{{ $role == 'admin' ? '/admin' : '/' }}'">
+                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9" />
+                    <path d="M9 22V12h6v10M2 10.6L12 2l10 8.6" />
                 </svg>
-
             </button>
+
 
         </div>
 
